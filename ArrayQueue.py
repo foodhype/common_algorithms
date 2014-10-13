a1b970bfb843c28f9d6d2c7eda79219aef898296
foodhype <brianschmitz.main@gmail.com>
2014-10-13 10:00:16 -0700

    ArrayQueue still buggy

--- ArrayQueue.py
+++ ArrayQueue.py
@@ -1,21 +1,66 @@
 class ArrayQueue(object):
     """Array implementation of FIFO queue."""
 
-    def __init__(self, capacity):
-        self.queue = [None for _ in xrange(capacity)]
+    # Still buggy; don't use.
+
+    def __init__(self):
+        self.capacity = 8
+        self.queue = [None for _ in xrange(self.capacity)]
         self.front = 0
         self.back = 0
-        self.capacity = capacity
         self.size = 0
 
+    def inorder(self):
+        index = self.front
+        if self.front <= self.back:
+            while index < self.back:
+                yield self.queue[index]
+                index = (index + 1) % self.capacity
+        else:
+            while index < self.size:
+                yield self.queue[index]
+                index = (index + 1) % self.capacity
+            index = 0
+            while index < self.back:
+                yield self.queue[index]
+                index = (index + 1) % self.capacity
+
     def push(self, val):
-        self.queue[self.back] = val
-        self.back = (self.back + 1) % self.capacity
+        if self.size >= 3 * self.capacity // 4:
+            temp = [None for _ in xrange(self.capacity * 2)]
+
+            index = 0
+            for elem in self.inorder():
+                temp[index] = elem
+                index += 1
+            temp[index] = val
+
+            self.queue = temp
+            self.capacity *= 2
+            self.front = 0
+            self.back = self.size
+        else:
+            self.queue[self.back] = val
+            self.back = (self.back + 1) % self.capacity
+
         self.size += 1
 
     def pop(self):
         if self.size == 0:
             raise Exception("Empty: Cannot pop.")
+        elif self.size <= self.capacity // 4:
+            print self.queue
+            temp = [None for _ in xrange(self.capacity // 2)]
+
+            for index, elem in enumerate(self.inorder()):
+                temp[index] = elem
+
+            self.queue = temp
+            self.capacity //= 2
+            self.front = 0
+            self.back = self.size
+            self.size -= 1
+            return self.queue[self.front]
         else:
             popped = self.queue[self.front]
             self.front = (self.front + 1) % self.capacity
@@ -24,21 +69,14 @@ class ArrayQueue(object):
 
 
 def main():
-    queue = ArrayQueue(5)
-    for i in xrange(5):
+    queue = ArrayQueue()
+    for i in xrange(32):
         queue.push(i)
 
-    assert queue.pop() == 0
-    assert queue.pop() == 1
+    for i in xrange(32):
+        queue.pop()
 
-    queue.push(10)
-    queue.push(11)
-
-    assert queue.pop() == 2
-    assert queue.pop() == 3
-    assert queue.pop() == 4
-    assert queue.pop() == 10
-    assert queue.pop() == 11
-
+    for i, elem in enumerate(queue.inorder()):
+        print elem
 
 main()            
