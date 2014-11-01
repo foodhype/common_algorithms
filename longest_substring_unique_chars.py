9396fdba7248a2553151c0948eb644e5653e7f6e
Brian Schmitz <bschmitz@cs.utexas.edu>
2014-11-01 12:57:21 -0500

    Added longest substring without repeating characters

---
+++ longest_substring_unique_chars.py
@@ -0,0 +1,42 @@
+def longest_substring_unique_chars(s):
+    """Find the longest substring of s containing only unique characters."""
+    if len(s) == 0:
+        return ""
+
+    end = 1
+    max_len = 1
+    cur_len = 1
+    visited = [None for _ in xrange(256)]
+    visited[ord(s[0])] = 0
+
+    for index in xrange(1, len(s)):
+        prev_index = visited[ord(s[index])]
+        if prev_index is None or index - cur_len > prev_index:
+            cur_len += 1
+        else:
+            if cur_len > max_len:
+                max_len = cur_len
+                end = index + 1
+            cur_len = index - prev_index
+
+        visited[ord(s[index])] = index
+
+    if cur_len > max_len:
+        max_len = cur_len
+        end = len(s)
+
+    return s[end - max_len:end]
+
+
+def main():
+    result = longest_substring_unique_chars("ABDEFGABEF")
+    assert result == "BDEFGA" or result == "DEFGAB"
+    result = longest_substring_unique_chars("GEEKSFORGEEKS")
+    assert result == "EKSFORG" or result == "KSFORGE"
+    assert longest_substring_unique_chars("BBBB") == "B"
+    assert longest_substring_unique_chars("") == ""
+    assert longest_substring_unique_chars("A") == "A"
+
+
+if __name__ == "__main__":
+    main()
