6d91bd127cd952aabf769f75f07329c66833389c
foodhype <brianschmitz.main@gmail.com>
2014-11-01 21:41:37 -0500

    fixed reservoir sampling

--- reservoir_sampling.py
+++ reservoir_sampling.py
@@ -1,19 +1,19 @@
 import random
 
 def sample(a, k):
-  """Get k random elements from a with uniform probability."""
-  reservoir = [a[i] for i in xrange(k)]
-  for i in xrange(k + 1, len(a)):
-    random_index = int(i * random.random())
-    if random_index < k:
-      reservoir[random_index] = a[i]
+    """Get k random elements from a with uniform probability."""
+    reservoir = [a[i] for i in xrange(k)]
+    for index in xrange(k, len(a)):
+        random_index = random.randint(0, index)
+        if random_index < k:
+            reservoir[random_index] = a[index]
 
-  return reservoir
+    return reservoir
 
 def main():
-  li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  print li
-  print sample(li, 3)
+    li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print li
+    print sample(li, 3)
 
 if __name__ == "__main__":
   main()
